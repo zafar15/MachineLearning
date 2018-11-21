2240d9ef367419c27fe20f68933d0ddd3910e922
zafar15 <mzafary.iqbal@yahoo.com>
2018-11-21 17:31:24 +0100

    Update kerasExample.py

--- kerasExample.py
+++ kerasExample.py
@@ -3,7 +3,11 @@ from keras.layers import Dense
 from keras.datasets import boston_housing
  
 (X_train, Y_train), (X_test, Y_test) = boston_housing.load_data()
- 
+
+# If you got an error about downloding of the database
+import ssl 
+ssl._create_default_https_context = ssl._create_unverified_context
+
 nFeatures = X_train.shape[1]
  
 model = Sequential()

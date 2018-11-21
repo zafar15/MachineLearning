edd69833451b0c374bc652d0e2bfa37ceb465ee0
zafar15 <mzafary.iqbal@yahoo.com>
2018-11-21 17:31:53 +0100

    Update kerasExample.py

--- kerasExample.py
+++ kerasExample.py
@@ -4,7 +4,7 @@ from keras.datasets import boston_housing
  
 (X_train, Y_train), (X_test, Y_test) = boston_housing.load_data()
 
-# If you got an error about downloding of the database
+# If you got an error about downloding of the database use that code
 import ssl 
 ssl._create_default_https_context = ssl._create_unverified_context
 
